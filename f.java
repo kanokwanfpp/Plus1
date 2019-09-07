f364e8f0245b7401e131b5e9dff251eceeca095a
kanokwanfpp <42598549+kanokwanfpp@users.noreply.github.com>
2019-09-07 21:51:29 +0700

    Add files via upload

---
+++ f.java
@@ -0,0 +1,11 @@
+public class f {
+ public static void main(String a[]) {
+int a1=Integer.parseInt(a[0]);
+int a2=Integer.parseInt(a[1]);
+int a3=Integer.parseInt(a[2]);
+System.out.println("plus="+(a1+a2));
+System.out.println("minus="+(a1-a2));
+System.out.println("multi="+(a1*a2));
+System.out.println("div="+(a1/a2));
+}
+}
